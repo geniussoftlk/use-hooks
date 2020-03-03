adae38b6328ef95afe761ea88645ca8e1733f379
Chathu Vishwajith <4evercreations@gmail.com>
2020-03-03 19:28:19 +0530

    Fix exporting and definistion files

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import useLocalStorage from './useLocalStorage';
 import useKeyPress from './useKeyPress';
+import useInterval from './useInterval';
 
-// eslint-disable-next-line import/prefer-default-export
-export { useLocalStorage, useKeyPress };
+export { useLocalStorage, useKeyPress, useInterval };
